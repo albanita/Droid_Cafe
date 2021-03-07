517d29402fd6579dfc579b4774488991c3084183
albanita <43749327+albanita@users.noreply.github.com>
2021-03-07 23:29:01 +0100

    tarea 4.2.2 acabada

--- app/src/main/java/com/example/droidcafe/OrderActivity.java
+++ app/src/main/java/com/example/droidcafe/OrderActivity.java
@@ -2,9 +2,13 @@ package com.example.droidcafe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -17,5 +21,26 @@ public class OrderActivity extends AppCompatActivity {
         String message = "Order: " + intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.order_textview);
         textView.setText(message);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+        switch (view.getId()){
+            case R.id.sameday:
+                displayToast(getString(R.string.same_day_messenger_service));
+                break;
+            case R.id.nextday:
+                displayToast(getString(R.string.next_day_ground_delivery));
+                break;
+            case R.id.pickup:
+                displayToast(getString(R.string.pick_up));
+                break;
+            default: break;
+        }
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message,
+                Toast.LENGTH_SHORT).show();
     }
 }
